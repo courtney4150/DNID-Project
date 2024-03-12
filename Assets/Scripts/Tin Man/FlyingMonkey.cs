b304a564c9e8b9e2acd802e9afff69bf1c2c8f1b
huf5 <152522942+huf5@users.noreply.github.com>
2024-03-12 16:39:09 -0400

    updates

---
+++ Assets/Scripts/Tin Man/FlyingMonkey.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingMonkey : MonoBehaviour
+{
+    [SerializeField] GameObject[] monkeyPrefab;
+    [SerializeField] float secondSpawn = 0.5f; 
+    [SerializeField] float minTras;
+    [SerializeField] float maxTras;
+
+    void Start(){
+        StartCoroutine(MonkeySpawn());
+    }
+
+    IEnumerator MonkeySpawn(){
+        while(true){
+            var wanted = Random.Range(minTras, maxTras);
+            var position = new Vector3(wanted, transform.position.y); 
+            GameObject gameObject = Instantiate(monkeyPrefab[Random.Range(0, monkeyPrefab.Length)], 
+            position, Quaternion.identity);
+            yield return new WaitForSeconds(secondSpawn);
+            Destroy(gameObject, 5f);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision){
+        if(collision.gameObject.tag == "Platform"){
+            Debug.Log("womp womp");
+            Destroy(gameObject);
+        }
+    }
+}
