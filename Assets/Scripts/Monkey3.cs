60d298b57499179cf24f9c90e6de891e990f3760
huf5 <152522942+huf5@users.noreply.github.com>
2024-03-15 17:13:00 -0400

    updates

---
+++ Assets/Scripts/Monkey3.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monkey3 : MonoBehaviour
+{
+    public float minSpeed = 3f;
+    public float maxSpeed = 15f;
+
+    public TinManController controller;
+
+    void Start()
+    {
+        transform.position = new Vector3(Random.Range(-5f, 5f), 10f, 0f);
+        float speed = Random.Range(minSpeed, maxSpeed);
+        GetComponent<Rigidbody2D>().velocity = Vector2.down * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name == "TinManSprite"){
+            Destroy(gameObject);
+            controller.TakeDamage(1);
+        }
+    }
+}
---
+++ Assets/Scripts/MonkeySpawn.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonkeySpawn : MonoBehaviour
+{
+    public GameObject[] fallingObjectPrefabs;
+    public float spawnInterval = 1f;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnObject", 0f, spawnInterval);
+    }
+
+    void SpawnObject()
+    {
+        // Randomly select a falling object prefab
+        GameObject prefab = fallingObjectPrefabs[Random.Range(0, fallingObjectPrefabs.Length)];
+
+        // Instantiate the selected prefab at the spawn point
+        Instantiate(prefab, transform.position, Quaternion.identity);
+    }
+}
+
+
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class FlyingMonkey : MonoBehaviour
+// {
+//     public GameObject[] monkeyPrefab;
+//     public float secondSpawn = 1f; 
+//     [SerializeField] float minTras;
+//     [SerializeField] float maxTras;
+
+//     //Calls the TMC
+//     public TinManController controller;
+
+//     void Start(){
+//         InvokeRepeating("SpawnObject", 0f, spawnInterval);
+//     }
+
+//     void SpawnObject()
+//     {
+//         // Randomly select a falling object prefab
+//         GameObject prefab = fallingObjectPrefabs[Random.Range(0, fallingObjectPrefabs.Length)];
+
+//         // Get the screen boundaries in world space
+//         float screenX = Random.Range(Camera.main.ScreenToWorldPoint(Vector3.zero).x, Camera.main.ScreenToWorldPoint(Vector3.right * Screen.width).x);
+
+//         // Calculate spawn position above the screen
+//         Vector3 spawnPosition = new Vector3(screenX, Camera.main.ScreenToWorldPoint(Vector3.up * Screen.height).y, 0f);
+
+//         // Instantiate the selected prefab at the random position
+//         Instantiate(prefab, spawnPosition, Quaternion.identity);
+
+//         Destroy(gameObject, 5f);
+//     }
+
+//     // IEnumerator MonkeySpawn(){
+//     //     while(true){
+//     //         var wanted = Random.Range(minTras, maxTras);
+//     //         var position = new Vector3(wanted, transform.position.y); 
+//     //         int random = Random.Range(0, monkeyPrefab.Length);
+//     //         Debug.Log("Prefab length is: " + monkeyPrefab.Length);
+//     //         Debug.Log("Random index: " + random);
+//     //         GameObject gameObject = Instantiate(monkeyPrefab[random], position, Quaternion.identity);
+//     //         yield return new WaitForSeconds(secondSpawn);
+//     //         Destroy(gameObject, 5f);
+//     //     }
+//     // }
+
+//     private void OnTriggerEnter2D(Collider2D collision){
+//         if(collision.gameObject.name == "TinManSprite"){
+//             Destroy(gameObject);
+//             controller.TakeDamage(1);
+//         }
+//     }
+// }
